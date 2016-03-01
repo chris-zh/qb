00f9e25cbca0ff7fc6d63d0a7307ebb775d006e4
chris-zh <chris.zh@outlook.com>
2016-03-01 21:52:57 +0800

    d8-3

--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, AnonymousUserMixin
@@ -121,9 +120,6 @@ class User(UserMixin, db.Model):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
     def gravatar(self, size=100, default='identicon', rating='g'):
         if request.is_secure:
             url = ''
@@ -132,6 +128,30 @@ class User(UserMixin, db.Model):
         # hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
         my_url = 'http://127.0.0.1:5000/static/favicon.ico'
         return my_url
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    @staticmethod
+    def generate_fake(count=100):
+        from sqlalchemy.exc import IntegrityError
+        from random import seed
+        import forgery_py
+        seed()
+        for i in range(count):
+            u = User(email=forgery_py.internet.email_address(),
+                     username=forgery_py.internet.user_name(True),
+                     password=forgery_py.lorem_ipsum.word(),
+                     confirmed=True,
+                     name=forgery_py.name.full_name(),
+                     location=forgery_py.address.city(),
+                     about_me=forgery_py.lorem_ipsum.sentence(),
+                     member_since=forgery_py.date.date(True))
+            db.session.add(u)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
 
 
 class AnonymousUser(AnonymousUserMixin):
@@ -142,12 +162,27 @@ class AnonymousUser(AnonymousUserMixin):
         return False
 
 
-login_manager.anonymous_user = AnonymousUser
-
-
 class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed, randint
+        import forgery_py
+
+        seed()
+        user_count = User.query.count()
+        for i in range(count):
+            u = User.query.offset(randint(0, user_count - 1)).first()
+            p = Post(body=forgery_py.lorem_ipsum.sentences(randint(1, 3)),
+                     timestamp=forgery_py.date.date(True),
+                     author=u)
+            db.session.add(p)
+            db.session.commit()
+
+
+login_manager.anonymous_user = AnonymousUser
