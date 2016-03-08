341f7e002aacb9232b778649862704160d7a6c14
chris.zh <chris.zh@outlook.com>
2016-03-08 09:53:09 +0800

    d9

--- app/models.py
+++ app/models.py
@@ -29,7 +29,7 @@ class Follow(db.Model):
     __tablename__ = 'follows'
     follower_id = db.Column(db.Integer, db.ForeignKey('users.id'),
                             primary_key=True)
-    followed_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+    followed_id = db.Column(db.Integer, db.ForeignKey('users.id'),
                             primary_key=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
